76a75d582907c28d5290eca42a8de53e27eccd1d
Shivam Shastrakar <103759041+ShivamShastrakar@users.noreply.github.com>
2026-08-20 09:39:38 +0530

    q

---
+++ feast/src/main/java/com/food_delivery/feast/user/entity/UserProfile.java
@@ -0,0 +1,4 @@
+package com.food_delivery.feast.user.entity;
+
+public class UserProfile {
+}
---
+++ feast/src/test/java/com/food_delivery/feast/FeastApplicationTests.java
@@ -0,0 +1,13 @@
+package com.food_delivery.feast;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeastApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
